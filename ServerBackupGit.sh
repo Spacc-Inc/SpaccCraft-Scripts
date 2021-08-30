67cf6c4b766bc3bcb14852f5f594761611aeb210
andrigamerita <37557992+andrigamerita@users.noreply.github.com>
2021-08-30 22:30:10 +0200

    Update ServerBackupGit.sh

--- ServerBackupGit.sh
+++ ServerBackupGit.sh
@@ -7,5 +7,5 @@ cd $ScriptDir
 ServerFolder="../../Beta-1.7.3"
 
 echo "Pushing to git..."
-cp -r "$ServerFolder/" "spacccraft-b1.7.3-backup/"
-cd "spacccraft-b1.7.3-backup/" && git add . && git commit -m "New backup ($(date +"%d-%m-%y %H:%M"))" && git push
+cp -r "$ServerFolder/" "spacccraft-b1.7.3-backup3/"
+cd "spacccraft-b1.7.3-backup3/" && git add . && git commit -m "New backup ($(date +"%d-%m-%y %H:%M"))" && git push
